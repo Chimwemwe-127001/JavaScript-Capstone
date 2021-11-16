1be3fca5d1e34e0040c93d176f014fb7cd5a28b5
Chimwemwe <sinyinzachimwemwe@gmail.com>
2021-11-16 12:09:01 +0200

    Create page header according to the wireframe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import Icon from './thunder.svg';
 
 const fetchApi = () => {
   fetch('https://api.openweathermap.org/data/2.5/weather?q=Lusaka&appid=623e557fbf15d070be5435e1d2494617')
@@ -6,3 +7,8 @@ const fetchApi = () => {
     .then((result) => console.log(result));
 };
 fetchApi();
+
+const navigation = document.querySelector('.navigation');
+const img = document.createElement('img');
+img.setAttribute('src', Icon);
+navigation.appendChild(img);
